67db7425b54d86f85a1abde4b44ce59cbe5b29f6
andres-martin <andresmartin266@gmail.com>
2019-04-08 15:51:58 +0000

    _strtok function created

--- testeo/holberton.h
+++ testeo/holberton.h
@@ -67,9 +67,9 @@ int permi(char **buff_tk, char *argv, int input_count, int *stat);
 int check_input(char *buff);
 char *_strdup(char *str);
 char **create_arg_list(char **buff_tk, char *buff, const char *delim);
-int token_len(char *str, char *delim);
-int count_tokens(char *str, char *delim);
-char **_strtok(char *line, char *delim);
+/* strtok collection */
+unsigned int check_match(char c, const char *str);
+char *_strtok(char *str, const char *delim);
 /* new prototypes*/
 char *_strcpy(char *dest, const char *src);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
