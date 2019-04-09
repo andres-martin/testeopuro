23fb80c7e09b3ea74a18d7d7e45ed1ff7273c819
andres-martin <andresmartin266@gmail.com>
2019-04-09 16:49:10 +0000

    betty applied to to_str and unset functions

--- testeo/unset.c
+++ testeo/unset.c
@@ -10,45 +10,44 @@
  * Return: 0 if successful 1 otherwise
  */
 int builtin_unsetenv(char **buff_tk, listint_t **env, char *buff,
-                char *argv, size_t input_count, int *stat)
+	char *argv, size_t input_count, int *stat)
 {
-        listint_t *temp = *env;
-        unsigned int index = 0;
-        int tokens = 0;
+	listint_t *temp = *env;
+	unsigned int index = 0;
+	int tokens = 0;
 
-        (void) buff;
-        while (buff_tk[tokens])
-                tokens++;
-        if (tokens < 2)
-        {
-                *stat = 0;
-                error_message(argv, input_count,
-                              ": Invalid Argument for unsetenv\n", buff_tk);
-        }
-        else
-        {
-                while (temp && _strcmp(buff_tk[1], temp->a))
-                {
-                        index++;
-                        temp = temp->next;
-                }
-                if (temp)
-                {
-                        if (delete_node_at_index(env, index) == -1)
-                        {
-                                error_message(argv, input_count,
-                                              ": unsetenv failed\n", buff_tk);
-                        }
-                }
-                else
-                {
-                        *stat = 1;
-                        error_message(argv, input_count,
-                                      ": Environment Variable not found\n",
-                                      buff_tk);
-                }
-                *stat = 0;
-        }
-        free(buff_tk);
-        return (1);
+	(void) buff;
+	while (buff_tk[tokens])
+		tokens++;
+	if (tokens < 2)
+	{
+		*stat = 0;
+		error_message(argv, input_count,
+			": Invalid Argument for unsetenv\n", buff_tk);
+	}
+	else
+	{
+		while (temp && _strcmp(buff_tk[1], temp->a))
+		{
+			index++;
+			temp = temp->next;
+		}
+		if (temp)
+		{
+			if (delete_node_at_index(env, index) == -1)
+			{
+				error_message(argv, input_count,
+					": unsetenv failed\n", buff_tk);
+			}
+		}
+		else
+		{
+			*stat = 1;
+			error_message(argv, input_count,
+				": Environment Variable not found\n", buff_tk);
+		}
+		*stat = 0;
+	}
+	free(buff_tk);
+return (1);
 }
